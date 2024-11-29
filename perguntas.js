46b14434ac5979711f98b4c73cb63417e744e7c5
Pensão Milionaria <00001114215065sp@al.educacao.sp.gov.br>
2024-11-29 15:46:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Servidor',
+    'Qual o melhor servidor de minecraft de hg?',
+    'Flamemc.com.br; gigante mlk'
+)
+
+criaCartao(
+    'Jogo',
+    'Qual o melhor jogo do mundo?',
+    'Minecraft, apenas'
+)
+
+criaCartao(
+    'Bebida',
+    'Qual a melhor bebida do mundo?',
+    'Água né bobão, vai se hidratar'
+)
+
+criaCartao(
+    'Russo',
+    'Como se diz Retroescavadeira em Russo?',
+    'I ghost the down cool'
+)
